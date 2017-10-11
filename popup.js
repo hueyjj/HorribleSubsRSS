40232f33176bc7100ca94bee97f1de894dad2266
huey <Huey Freeman>
2017-10-10 19:48:05 -0700

    firefox

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
-function getRSS(url) {
+function loadRSS(url) {
     $.get(url, function(data) { 
-        let rss = []:
+        let rss = [];
         var $XML = $(data);
         $XML.find("item").each(function() {
             var $this = $(this),
@@ -13,11 +13,51 @@ function getRSS(url) {
             };
             rss.push(item);
         });
+
+        let table = convertToTable(rss);
+        load(table);
     });
-    return (rss.length != 0) ? rss : null;
 }
 
+//TODO Simply list down with same names and put magnet links in the same row
+function convertToTable(rss) {
+    if (rss.length > 0) {
+        let table = document.createElement("table");
+        table.className = "rss-table";
+
+        rss.forEach(function(item) {
+            let row = document.createElement("tr");
+            row.className = "rss-row";
+            row.insertCell(-1);
+            row.insertCell(-1);
+
+
+            let title = document.createTextNode(rss.title);
+            title.className = "rss-title";
+
+            let magnet = document.createTextNode(rss.link);
+            magnet.className = "rss-magnet";
+
+            row.appendChild(title);
+            row.appendChild(magnet);
+            
+            table.insertRow(-1);
+            table.appendChild(row);
+        });
+        return table;
+    }
+    else return null;
+}
+
+function load(table) {
+    if (table != null) {
+        // load new rss html document
+        document.body.appendChild(table);
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
-    const let url = "http://horriblesubs.info/rss.php?res=all";
-    let rss = getRSS(url);
+    const url = "http://horriblesubs.info/rss.php?res=all";
+    loadRSS(url);
 });
